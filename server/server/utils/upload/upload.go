b99699df78eddd653d489aa6d6fdf5e8c1f0f6a6
sockstack <85564748@qq.com>
2021-01-27 19:20:23 +0800

    add

---
+++ server/server/utils/upload/upload.go
@@ -0,0 +1,34 @@
+package upload
+
+import (
+	"gin-vue-admin/global"
+	"mime/multipart"
+)
+
+//@author: [ccfish86](https://github.com/ccfish86)
+//@author: [SliverHorn](https://github.com/SliverHorn)
+//@interface_name: OSS
+//@description: OSS接口
+
+type OSS interface {
+	UploadFile(file *multipart.FileHeader) (string, string, error)
+	DeleteFile(key string) error
+}
+
+//@author: [ccfish86](https://github.com/ccfish86)
+//@author: [SliverHorn](https://github.com/SliverHorn)
+//@function: NewOss
+//@description: OSS接口
+//@description: OSS的实例化方法
+//@return: OSS
+
+func NewOss() OSS {
+	switch global.GVA_CONFIG.System.OssType {
+	case "local":
+		return &Local{}
+	case "qiniu":
+		return &Qiniu{}
+	default:
+		return &Local{}
+	}
+}
